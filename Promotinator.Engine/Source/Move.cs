c05b8c52139f19de614e144bf101fc35cc87b602
Nik Radi <nik.radi@live.dk>
2025-04-27 21:23:59 +0200

    Add iterative deepening

--- Promotinator.Engine/Source/Move.cs
+++ Promotinator.Engine/Source/Move.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Promotinator.Engine;
 
-public struct Move {
+public struct Move : IEquatable<Move> {
     public Coord From;
     public Coord To;
     public Piece? CapturedPiece;
@@ -18,13 +20,32 @@ public struct Move {
         return str;
     }
 
+    public override int GetHashCode() {
+        return From.GetHashCode() ^ To.GetHashCode();
+    }
+
+    public override bool Equals(object obj) {
+        return obj is Move && Equals(obj);
+    }
+
+    public bool Equals(Move other) {
+        return
+            From == other.From &&
+            To == other.To &&
+            CapturedPiece.HasValue == other.CapturedPiece.HasValue &&
+            IsEnPassantCapture == other.IsEnPassantCapture &&
+            IsKingsideCastling == other.IsKingsideCastling &&
+            IsQueensideCastling == other.IsQueensideCastling &&
+            PromotionType == other.PromotionType;
+    }
+
     private string ToString(PromotionType type) {
         switch (type) {
             case PromotionType.Queen: return "q";
             case PromotionType.Rook : return "r";
             case PromotionType.Knight: return "n";
             case PromotionType.Bishop: return "b";
-            default: return null;
+            default: return string.Empty;
         }
     }
 }
--- Promotinator.Engine/Source/Search.cs
+++ Promotinator.Engine/Source/Search.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Promotinator.Engine;
 
 public struct ScoredMove {
@@ -6,25 +8,53 @@ public struct ScoredMove {
 }
 
 public static class Search {
+    private static bool _isDone;
+    private static int _maxMilliseconds;
+    private static Stopwatch _stopwatch = new();
+
     public static async Task<List<ScoredMove>> FindBestMoveAsync(Board board) {
         return await Task.Run(() => FindBestMove(board));
     }
 
-    public static List<ScoredMove> FindBestMove(Board board) {
+    public static List<ScoredMove> FindBestMove(Board board, int maxMilliseconds = 500) {
         SearchDebug.ClearLog();
 
         List<ScoredMove> result = [];
         List<Move> moves = MoveGenerator.GenerateMoves(board);
 
-        foreach (Move move in moves) {
-            BoardState state = board.MakeMove(move);
+        if (moves.Count == 1) {
+            result.Add(new() { Move = moves[0]} );
+            return result;
+        }
 
-            int score = Minimax(board, 3);
-            result.Add(new() { Move = move, Score = score });
+        Dictionary<Move, ScoredMove> cache = new();
+        foreach (var move in moves) {
+            cache[move] = new() { Move = move, Score = 0 };
+        }
 
-            SearchDebug.Log($"Move:{move} Score:{score}");
+        // Iterative deepening
+        int depth = 1;
+        _isDone = false;
+        _maxMilliseconds = maxMilliseconds;
+        _stopwatch.Restart();
 
-            board.UndoMove(move, state);
+        while (!_isDone) {
+            foreach (Move move in moves) {
+                BoardState state = board.MakeMove(move);
+
+                int score = Minimax(board, depth);
+                cache[move] = new() { Move = move, Score = score };
+
+                SearchDebug.Log($"Move:{move} Score:{score}");
+
+                board.UndoMove(move, state);
+            }
+
+            depth += 1;
+        }
+
+        foreach (var move in cache.Values) {
+            result.Add(move);
         }
 
         result.Sort((m1, m2) => m2.Score.CompareTo(m1.Score));
@@ -33,6 +63,11 @@ public static class Search {
     }
 
     private static int Minimax(Board board, int depth) {
+        if (_stopwatch.ElapsedMilliseconds >= _maxMilliseconds) {
+            _isDone = true;
+            return 0;
+        }
+
         if (depth == 0) {
             return Eval.Score(board);
         }
